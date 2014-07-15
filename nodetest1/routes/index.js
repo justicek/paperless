7ae1b78b53dec6a0abb8a86e4dc2ee0c30b44164
kjustice <kjustice@sagepath.com>
2014-07-15 14:35:00 -0400

    Views not staged for commit

--- nodetest1/routes/index.js
+++ nodetest1/routes/index.js
@@ -16,6 +16,11 @@ router.get('/login', function(req, res) {
   	res.render('login', {});
 });
 
+/* GET logout page */
+router.get('/logout', function(req, res) {
+	res.render('logout', {});
+})
+
 /* POST login */
 router.post('/login', passport.authenticate('local'),
 	function(req, res) {
@@ -31,7 +36,7 @@ router.post('/login', passport.authenticate('local'),
 router.get('/home', function(req, res) {
 	res.render('home', {
 		title: 'paperless',
-		user: req.session.user
+		sessionuser: req.session.user
 	});
 });
 
@@ -45,7 +50,40 @@ router.get('/new', function(req, res) {
 
 /* GET control panel page */
 router.get('/cp', function(req, res) {
-	// todo //
+	res.render('cp', {
+		sessionuser: req.session.user
+	});
+});
+
+/* POST control panel page */
+router.post('/cp', function(req, res) {
+	var prevUsername = req.body.hiddenfield;
+	var user = {};
+	user.username = req.body.username;
+	user.email = req.body.email;
+	user.password = req.body.password;
+		
+
+	req.db.get('usercollection').update(
+		{ username: prevUsername },
+		{ username: user.username,
+		  email: user.email,
+		  password: user.password },
+		{ upsert: false }
+	);
+
+	res.render('cp', {
+		sessionuser: user
+	});
 });
 
 module.exports = router;
+
+// route middleware
+function isLoggedIn(req, res, next) {
+
+	if (req.isAuthenticated())
+		return next;
+
+	res.redirect('/');
+}
